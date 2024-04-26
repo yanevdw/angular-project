7f0ff509dc43fde50384e663ffa1dc94ee130b1e
Yané van der Westhuizen <yanevdw2@gmail.com>
2024-04-26 12:41:09 +0200

    feat: :sparkles: Added some database/firestore integration to the collections page.
    Began implementation of the collections component by getting the id of the user's bookshelf from the bookshelf table in the database.

--- src/app/components/collections/collections.component.ts
+++ src/app/components/collections/collections.component.ts
@@ -1,6 +1,7 @@
 import { CollectionDisplayComponent } from './components/collection-display/collection-display.component';
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { DataService } from '../../data.service';
 
 @Component({
   selector: 'app-collections',
@@ -9,4 +10,19 @@ import { RouterLink } from '@angular/router';
   templateUrl: './collections.component.html',
   styleUrl: './collections.component.scss',
 })
-export class CollectionsComponent {}
+export class CollectionsComponent implements OnInit {
+  bookshelfId: number | undefined;
+  dataService = inject(DataService);
+  bookshelfResult$ = this.dataService.getBookshelf();
+
+  ngOnInit() {
+    let bookshelfSubscription = this.bookshelfResult$.subscribe((bookshelf) => {
+      this.bookshelfId = bookshelf[0].id;
+    });
+
+    // Unsubscribe after the id has been fetched.
+    if (bookshelfSubscription && this.bookshelfId) {
+      bookshelfSubscription.unsubscribe();
+    }
+  }
+}
