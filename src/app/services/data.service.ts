469045e507efee8de8de8b66db9815deb1b3abe6
Yané van der Westhuizen <yanevdw2@gmail.com>
2024-04-29 10:09:47 +0200

    feat: :recycle: Moved data service to correct directory
    Moved data service to services directory.

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -6,30 +6,27 @@ import {
   query,
   where,
 } from '@angular/fire/firestore';
-
-import { AuthService } from './services/auth.service';
-import { Book, BookShelf } from './models/states';
+import { Book, BookShelf } from '../models/states';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataService {
-  authService = inject(AuthService);
   firestore = inject(Firestore);
 
   // Get the bookshelf associated with the user, using the current (logged in) user's uid.
-  getBookshelf() {
+  getBookshelf(userId: string) {
     const bookshelfQuery = query(
       collection(this.firestore, 'bookshelf'),
-      where('userId', '==', this.authService.getCurrentUser?.uid),
+      where('userId', '==', userId),
     );
     return collectionData(bookshelfQuery, { idField: 'id' }) as Observable<
       BookShelf[]
     >;
   }
 
-  // Get the books in a users bookshelf
+  // Get the books in a users bookshelf.
   getBooks(bookshelfId: number) {
     const booksQuery = query(
       collection(this.firestore, 'book'),
