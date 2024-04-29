c2a308447d23e51d5282c2c30f765e566ef1d839
Yané van der Westhuizen <yanevdw2@gmail.com>
2024-04-29 13:09:26 +0200

    feat: :sparkles: Added functionality to get the saved cookie.
    Added a function that gets the current user's id from the cookie.

--- src/app/utils/utils.ts
+++ src/app/utils/utils.ts
@@ -10,3 +10,15 @@ export function deleteCookie(): void {
   document.cookie =
     'currentUserId=; expires=Mon, 28 Apr 2024 00:00:00 UTC; path=/;';
 }
+
+export function getCookie() {
+  const cookie = decodeURIComponent(document.cookie);
+  const cookieSections = document.cookie.split(';');
+  let cookieValue = cookieSections[0];
+  cookieValue = cookieValue.substring(
+    cookieValue.indexOf('=') + 1,
+    cookieValue.length,
+  );
+
+  return cookieValue;
+}
