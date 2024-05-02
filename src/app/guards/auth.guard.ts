b7443a6eba873db99250c39501caa77bb9e04d78
Yané van der Westhuizen <yanevdw2@gmail.com>
2024-05-02 20:16:51 +0200

    feat: :fire: Removed unused code
    Removed code that has been commented out.

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -7,19 +7,9 @@ export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
-  // if (authService.currentUserSignal()) {
-  //   authService.isUserSet$.next(true);
-  //   return router.parseUrl('login');
-  // } else {
-  //   authService.isUserSet$.next(true);
-  //   return true;
-  // }
-  // // return true;
-
   authService.currentUser$.pipe(take(1)).subscribe((user) => {
     if (user) {
       authService.currentUserSignal.set({
-        //! is added because it is pretty much guaranteed that the user is logged in at this point
         email: user.email!,
         name: user.displayName!,
         id: user.uid!,
@@ -32,7 +22,6 @@ export const authGuard: CanActivateFn = (route, state) => {
       router.navigate(['/login']);
       return false;
     } else {
-      // router.navigate(['/']);
       authService.isUserSet$.next(true);
       return true;
     }
