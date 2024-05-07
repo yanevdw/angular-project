1d8471d3737d4ba82a07981390faa04f057f5411
Yané van der Westhuizen <yanevdw2@gmail.com>
2024-05-07 21:53:15 +0200

    feat: :fire: Remove unneeded code
    Removed localStorage code since it is no longer used.

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -65,7 +65,6 @@ export class AuthService {
   logout() {
     return from(
       signOut(this.firebaseAuth).then(() => {
-        localStorage.clear();
         this.router.navigate(['/login']);
       }),
     );
