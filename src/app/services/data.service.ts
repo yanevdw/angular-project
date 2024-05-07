24ca2a94b9ed026055ecc8be06050c11e226f346
Yané van der Westhuizen <yanevdw2@gmail.com>
2024-05-07 21:54:45 +0200

    feat: :recycle: Updated services to incorporate new state management
    Added function to filter results to align with bookshelf state.

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -7,8 +7,8 @@ import {
   query,
   where,
 } from '@angular/fire/firestore';
-import { Book, BookShelf } from '../models/states';
-import { from, Observable } from 'rxjs';
+import { Book, Bookshelf, BookShelf } from '../models/states';
+import { from, map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -27,13 +27,45 @@ export class DataService {
     }) as Observable<BookShelf[]>;
   }
 
-  // Get the books in a users bookshelf.
-  getBooks(bookshelfId: number) {
+  // Get the books in a user's bookshelf.
+  getBooks(bookshelfId: string) {
     const booksQuery = query(
       collection(this.firestore, 'book'),
       where('bookshelfId', '==', bookshelfId.toString()),
     );
     return collectionData(booksQuery) as Observable<Book[]>;
+  }
+
+  filterBooks(bookshelfId: string) {
+    return this.getBooks(bookshelfId).pipe(
+      // switchMap((books: Book[]) => {
+      map((books) => {
+        if (books) {
+          let bookshelf: Bookshelf = {} as Bookshelf;
+          bookshelf.tbr = books.filter(
+            (book: Book) => book.bookshelf_category === 'tbr',
+          );
+          bookshelf.dnf = books.filter(
+            (book: Book) => book.bookshelf_category === 'dnf',
+          );
+          bookshelf.read = books.filter(
+            (book: Book) => book.bookshelf_category === 'read',
+          );
+          bookshelf.currentlyReading = books.filter(
+            (book: Book) => book.bookshelf_category === 'currently reading',
+          );
+          if (
+            bookshelf.tbr ||
+            bookshelf.dnf ||
+            bookshelf.read ||
+            bookshelf.currentlyReading
+          ) {
+            return bookshelf as Bookshelf;
+          }
+        }
+        return {} as Bookshelf;
+      }),
+    );
   }
 
   // Create a bookshelf for a new user.
