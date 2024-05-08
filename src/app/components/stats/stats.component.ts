57e122ca9b3b94288e8cea9425be860a703cfa3e
Yané van der Westhuizen <yanevdw2@gmail.com>
2024-05-08 13:09:39 +0200

    feat: :recycle: Integrated state
    Integrated state into the stats component.

--- src/app/components/stats/stats.component.ts
+++ src/app/components/stats/stats.component.ts
@@ -1,53 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
+import { Store } from '@ngrx/store';
+import { CurrentBookshelfState } from '../../store/reducer';
+import { selectedBookshelf } from '../../store/selectors';
+import { Subscription } from 'rxjs';
 import { Book } from '../../models/states';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-stats',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, AsyncPipe],
   templateUrl: './stats.component.html',
   styleUrl: './stats.component.scss',
 })
-export class StatsComponent {
-  currentBooks = JSON.parse(localStorage.getItem('books') as string);
-  currentlyReading = this.currentBooks.filter(
-    (book: Book) => book.bookshelf_category === 'currently reading',
-  );
-  tbr = this.currentBooks.filter(
-    (book: Book) => book.bookshelf_category === 'tbr',
-  );
-  dnf = this.currentBooks.filter(
-    (book: Book) => book.bookshelf_category === 'dnf',
-  );
-  read = this.currentBooks.filter(
-    (book: Book) => book.bookshelf_category === 'read',
-  );
+export class StatsComponent implements OnInit, OnDestroy {
+  bookStore = inject(Store<CurrentBookshelfState>);
+  currentBooks$ = this.bookStore.select(selectedBookshelf);
+  bookshelfSubscription: Subscription | undefined = undefined;
 
-  calculateTotalPagesRead() {
+  ngOnInit() {
+    this.bookshelfSubscription = this.currentBooks$.subscribe();
+  }
+
+  calculateTotalBooks(
+    currentBooks: Book[],
+    read: Book[],
+    tbr: Book[],
+    dnf: Book[],
+  ): number {
+    return currentBooks.length + read.length + tbr.length + dnf.length;
+  }
+
+  calculateTotalPagesRead(
+    currentBooks: Book[],
+    readBooks: Book[],
+    dnfBooks: Book[],
+  ) {
     let totalPages = 0;
 
-    for (const book of this.read) {
+    for (const book of readBooks) {
       totalPages += Number(book.pages);
     }
 
-    for (const book of this.currentlyReading) {
+    for (const book of dnfBooks) {
+      totalPages += Number(book.pages);
+    }
+
+    for (const book of currentBooks) {
       totalPages += Number(book.pages_read);
     }
 
     return totalPages;
   }
 
-  calculateFavouritePace() {
+  calculateFavouritePace(
+    currentBooks: Book[],
+    readBooks: Book[],
+    tbrBooks: Book[],
+    dnfBooks: Book[],
+  ) {
+    let books = this.combineBooks(currentBooks, readBooks, tbrBooks, dnfBooks);
+
     let count = 0;
     let favouritePace = '';
 
-    for (const book of this.currentBooks) {
+    for (const book of books) {
       if (book.pace != '') {
         let paceFrequency = 0;
 
-        for (const storedBook of this.currentBooks) {
+        for (const storedBook of books) {
           if (book.pace === storedBook.pace) {
             paceFrequency++;
           }
@@ -63,15 +86,21 @@ export class StatsComponent {
     return favouritePace;
   }
 
-  calculateFavouriteCategory() {
+  calculateFavouriteCategory(
+    currentBooks: Book[],
+    readBooks: Book[],
+    tbrBooks: Book[],
+    dnfBooks: Book[],
+  ) {
     let count = 0;
     let favouriteCategory = '';
+    let books = this.combineBooks(currentBooks, readBooks, tbrBooks, dnfBooks);
 
-    for (const book of this.currentBooks) {
+    for (const book of books) {
       if (book.category != '') {
         let catFrequency = 0;
 
-        for (const storedBook of this.currentBooks) {
+        for (const storedBook of books) {
           if (book.category === storedBook.category) {
             catFrequency++;
           }
@@ -113,15 +142,47 @@ export class StatsComponent {
   //   return favouriteMood;
   // }
 
-  calculateAverageRating() {
+  calculateAverageRating(
+    currentBooks: Book[],
+    readBooks: Book[],
+    tbrBooks: Book[],
+    dnfBooks: Book[],
+  ) {
     let count = 0;
     let totalRating = 0;
+    let books = this.combineBooks(currentBooks, readBooks, tbrBooks, dnfBooks);
 
-    for (const book of this.currentBooks) {
+    for (const book of books) {
       count++;
       totalRating += book.rating;
     }
 
-    return totalRating / count;
+    return (totalRating / count).toFixed(2);
+  }
+
+  combineBooks(
+    currentBooks: Book[],
+    readBooks: Book[],
+    tbrBooks: Book[],
+    dnfBooks: Book[],
+  ) {
+    let books: Book[] = [];
+    for (const book of readBooks) {
+      books.push(book);
+    }
+    for (const book of currentBooks) {
+      books.push(book);
+    }
+    for (const book of tbrBooks) {
+      books.push(book);
+    }
+    for (const book of dnfBooks) {
+      books.push(book);
+    }
+    return books;
+  }
+
+  ngOnDestroy(): void {
+    this.bookshelfSubscription?.unsubscribe();
   }
 }
