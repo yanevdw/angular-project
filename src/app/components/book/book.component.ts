e5fc71f589447a5e44a23c6ec4a59b0467c00637
Yané van der Westhuizen <yanevdw2@gmail.com>
2024-05-10 13:43:16 +0200

    feat: :recycle: Updated implementation of collections and books
    Edited styles, added additional functionality and fixes.

--- src/app/components/book/book.component.ts
+++ src/app/components/book/book.component.ts
@@ -1,12 +1,16 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { DataService } from '../../services/data.service';
-import { Observable, take } from 'rxjs';
+import { Observable, Subscription, take } from 'rxjs';
 import { Book } from '../../models/states';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
 
 import { AsyncPipe } from '@angular/common';
-import { matDelete, matEdit } from '@ng-icons/material-icons/baseline';
+import {
+  matDelete,
+  matEdit,
+  matKeyboardBackspace,
+} from '@ng-icons/material-icons/baseline';
 import { AddBookComponent } from '../explore/components/add-book/add-book.component';
 import { EditBookComponent } from './components/edit-book/edit-book.component';
 import { DeleteBookComponent } from './components/delete-book/delete-book.component';
@@ -22,20 +26,26 @@ import { NzButtonComponent } from 'ng-zorro-antd/button';
     EditBookComponent,
     DeleteBookComponent,
     NzButtonComponent,
+    RouterLink,
   ],
-  viewProviders: [provideIcons({ matDelete, matEdit })],
+  viewProviders: [provideIcons({ matDelete, matEdit, matKeyboardBackspace })],
   templateUrl: './book.component.html',
   styleUrl: './book.component.scss',
 })
-export class BookComponent implements OnInit {
+export class BookComponent implements OnInit, OnDestroy {
   activeRoute = inject(ActivatedRoute);
   selectedBook = this.activeRoute.snapshot.paramMap.get('bookIsbn');
   dataService = inject(DataService);
   bookResult$: Observable<Book[]> | undefined;
+  bookSubscription: Subscription | undefined;
   book: string[] | undefined = undefined;
 
   ngOnInit() {
     this.bookResult$ = this.dataService.getBook(this.selectedBook ?? '');
-    this.bookResult$.pipe(take(2)).subscribe();
+    this.bookSubscription = this.bookResult$.pipe(take(2)).subscribe();
+  }
+
+  ngOnDestroy() {
+    this.bookSubscription?.unsubscribe();
   }
 }
