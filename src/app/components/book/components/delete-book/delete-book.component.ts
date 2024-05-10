a928033b951b8e5e615a872a77d91a3c994c7e8b
Yané van der Westhuizen <yanevdw2@gmail.com>
2024-05-10 13:46:15 +0200

    feat: :recycle: Updated delete component implementation
    Edit style and integrated delete functionality

--- src/app/components/book/components/delete-book/delete-book.component.ts
+++ src/app/components/book/components/delete-book/delete-book.component.ts
@@ -1,7 +1,9 @@
-import { Component, inject, Input } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NgIcon } from '@ng-icons/core';
 import { DataService } from '../../../../services/data.service';
+import { Observable, take } from 'rxjs';
+import { Book } from '../../../../models/states';
 
 @Component({
   selector: 'app-delete-book',
@@ -10,14 +12,24 @@ import { DataService } from '../../../../services/data.service';
   templateUrl: './delete-book.component.html',
   styleUrl: './delete-book.component.scss',
 })
-export class DeleteBookComponent {
+export class DeleteBookComponent implements OnInit {
   @Input() bookIsbn: string | undefined | null;
   dataService = inject(DataService);
+  currentBook$: Observable<Book[]> | undefined;
+
+  ngOnInit() {
+    if (this.bookIsbn) {
+      this.currentBook$ = this.dataService.getBookId(this.bookIsbn);
+    }
+  }
 
   handleDeleteClick() {
-    if (this.bookIsbn) {
-      console.log('Here');
-      this.dataService.deleteBook(this.bookIsbn);
+    if (this.currentBook$) {
+      this.currentBook$.pipe(take(2)).subscribe((result) => {
+        if (result) {
+          this.dataService.deleteBook(result[0].id);
+        }
+      });
     } else {
       //TODO: Add feedback saying the book can't be deleted
     }
